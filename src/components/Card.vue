1ad56eecde6c5d41ce1dec81cf037730131820ed
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-12 15:12:44 +0100

    Added Card component in Main component and print every card in dc-comics.json

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card-col">
+    <div class="img-container">
+      <img
+        :src="img"
+        :alt="title"
+      />
+    </div>
+    <p class="card-title"> {{title}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props:  ["img", "title"]
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/variables.scss";
+.card-col {
+  width: calc(100% / 6 - $padding-x * 2.05);
+
+  .img-container {
+    height: $max-width-container / 6;
+    overflow: hidden;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .card-title{
+      padding: $padding-y 0;
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,35 @@
 <template>
   <main>
+    <div class="main-hero">
+
+    </div>
+
     <div class="container">
-      <p>--> Content goes here &#60;--</p>
+      <div class="label-cs">
+        CURRENT SERIES
+      </div>
+
+      <Card v-for="(card, i) in dcList" :key="i" :img="card.thumb" :title="card.series.toUpperCase()">
+        
+      </Card>
     </div>
   </main>
 </template>
 
 <script>
-import jsonData from '@/assets/dc-comics.json'
+import jsonData from "@/assets/dc-comics.json";
+import Card from "./Card.vue"
 
 export default {
   name: "Main",
-  data(){
+  components: {
+    Card
+  },
+  data() {
     return {
       dcList: jsonData,
-    }
-  }
+    };
+  },
 };
 </script>
 
